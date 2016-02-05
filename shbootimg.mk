c4b62fe4a141a5e43f99ede1fc76038122f3b839
Shilin Victor <chrono.monochrome@gmail.com>
2016-02-05 17:00:29 +0700

    Revert "Use precompiled recovery ramdisk for now"
    This reverts commit 144103e8fa2808eda713929147e362e2dec1b457.

--- shbootimg.mk
+++ shbootimg.mk
@@ -16,17 +16,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ARM_CROSS_COMPILE := CROSS_COMPILE="/home/chronomonochrome/root/prebuilts/misc/linux-x86/ccache/ccache /home/chronomonochrome/root/prebuilts/gcc/linux-x86/arm/armv7a-linux-gnueabihf-5.2/bin/armv7a-linux-gnueabihf-"
-
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
-RECOVERY_RAMDISK := device/samsung/u8500-common/prebuilt/ramdisk-recovery.cpio
-
-TARGET_KERNEL_BINARIES: $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL) $(RECOVERY_RAMDISK) $(uncompressed_ramdisk)
+TARGET_KERNEL_BINARIES: $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL) $(recovery_uncompressed_ramdisk) $(uncompressed_ramdisk)
 	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) $(TARGET_PREBUILT_INT_KERNEL_TYPE)
 	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) modules
 	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) INSTALL_MOD_PATH=../../$(KERNEL_MODULES_INSTALL) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) modules_install
